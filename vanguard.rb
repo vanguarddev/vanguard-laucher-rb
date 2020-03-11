3e1490fa72d5f810f84d2f5368ba4062068140fb
rigel-EVA <eva@rigel.moe>
2020-03-10 20:04:32 -0700

    wee bit of cleanup
    also added Launcher Options class, for sanity

--- vanguard.rb
+++ vanguard.rb
@@ -1,7 +1,59 @@
 require 'nokogiri'
 require 'rest-client'
 require 'fileutils'
-
+class Launcher_Options
+    def initialize
+        @name=""
+        @exec=""
+        @order=-1
+        @params=""
+        @architecture=""
+    end
+    def name
+        return @name
+    end
+    def exec
+        return @exec
+    end
+    def order
+        return @order
+    end
+    def params
+        return @params
+    end
+    def architecture
+        return @architecture
+    end
+    def name=(new_n)
+        @name=new_n
+    end
+    def exec=(new_e)
+        @exec=new_e
+    end
+    def order=(new_o)
+        @order=new_o
+    end
+    def params=(new_p)
+        @params=new_p
+    end
+    def architecture=(new_a)
+        @architecture=new_a
+    end
+    def execute_string
+        return "#{@exec} #{@params}"
+    end
+    class << self
+        def from_nokogiri(noko_node)
+            returner=Launcher_Options.new
+            returner.exec=noko_node.xpath("@exec").to_s
+            returner.order=noko_node.xpath("@order").to_s.to_i
+            returner.params=noko_node.xpath("@params").to_s
+            returner.architecture=noko_node.xpath("@architecture").to_s
+            returner.name=noko_node.xpath("text()").to_s
+            return returner
+        end
+    end
+end
 class File_dl
     def initialize
         @location=""
@@ -32,21 +84,38 @@ class File_dl
         returner=returner[0..-3]#Hacky way of getting rid of that last character... a bit dumb, but it's hacky for a reason
         returner+="\n\t\]\n\t\"md5\"\:\"#{md5}\"\n\}"
     end
+    def to_toml()
+        # returner= "\[\[file\]\]\n"
+        # returner+="name = \"#{@location}\"\n"
+        # returner+="url  = \[#{urls.each{|u| '"'+u+'"'}\]\n"
+        # retruner+="md5  = \"#{@md5}\"\n"
+        return returner
+    end
+    class << self
+        def from_nokogiri(noko_node)
+            returner=File_dl.new
+            returner.location=noko_node.xpath("@name").to_s
+            returner.md5=noko_node.xpath("@md5").to_s
+            noko_node.xpath("url/text()").each{|u| returner.urls.push(u.to_s.split(/\s/)[0])}
+            return returner
+        end
+    end
 end
 
 def parseFileList(nokogiri)
-    output=[]#Getting rid of any data in output from before this ...
+    returner=[]
     nokogiri.xpath("/manifest/filelist/file").each{|f|
-        output.push(File_dl.new)
-        output[-1].location=f.xpath("@name").to_s
-        output[-1].md5=f.xpath("@md5").to_s
-        f.xpath("url/text()").each{|u|
-            output[-1].urls.push(u.to_s)
-        }
+        returner.push(File_dl.from_nokogiri(f))
     }
-    return output
+    return returner
 end
-
+def parseLaunchers(nokogiri)
+    returner=[]
+    nokogiri.xpath("/manifest/profiles/launch").each {|l|
+        returner.push(Launcher_Options.from_nokogiri(l))
+    }
+    return returner
+end
 def download_file(file_uris, file_name, md5)
     dirs=file_name.split('/')[0..-2].join('/')
     unless(dirs.length==0)
@@ -82,9 +151,17 @@ if(__FILE__==$0)
     puts FileUtils.pwd()
     boop.each{|b|
         #puts b.to_json
+        puts b.urls
+        puts host_name
+        puts b.location
         download_file(b.urls,"#{host_name}/#{b.location}", b.md5)
     }
     launch_options=noko_xml.xpath("/manifest/profiles/launch")
     #for the sake of sanity we are going to just autolaunch the first one
-    exec("wine \"./#{host_name}/#{launch_options[0].xpath("@exec").to_s}\" #{launch_options[0].xpath("@params").to_s}")
+    launch="\"./#{host_name}/#{launch_options[0].xpath("@exec").to_s}\" #{launch_options[0].xpath("@params").to_s}"
+    if ((/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil)
+        exec(launch)
+    else
+        exec("wine #{launch}")
+    end
 end
